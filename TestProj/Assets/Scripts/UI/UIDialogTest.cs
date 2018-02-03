66a0707dd39c8690389d7c316c5dca66ec7db83f
zwz <zwz@veewo>
2018-02-03 11:30:21 +0800

    no message

--- TestProj/Assets/Scripts/UI/UIDialogTest.cs
+++ TestProj/Assets/Scripts/UI/UIDialogTest.cs
@@ -1,4 +1,4 @@
-//================Generate Time : 02/03/2018 11:10:00================
+//================Generate Time : 02/03/2018 11:27:17================
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +7,6 @@ using UnityEngine.UI;
 
 public class UIDialogTest : UIDialog {
 	//=====================  Auto Generate UI Code Start, DO NOT MODIFY !========================
-	public Image m_Image1;
-	public Image m_Image2;
-	public Image m_Image3;
+	public Image m_img1;
 	//=====================  Auto Generate UI Code End  ========================
-
-	void OnEnable () {
-		vee.Utils.DebugLogInEditor ("UIDialogTest  onenable");
-		this.m_Image1.gameObject.transform.Translate (new Vector3(0, 100, 0));
-		this.m_Image2.gameObject.transform.Translate (new Vector3(0, -100, 0));
-	}
-
-	void OnDisable () {
-		vee.Utils.DebugLogInEditor ("UIDialogTest  OnDisable");
-		this.m_Image1.gameObject.transform.Translate (new Vector3(0, -100, 0));
-		this.m_Image2.gameObject.transform.Translate (new Vector3(0, 100, 0));
-	}
 }
